9fc5dfefbba13d04a456f7386df62dbaa30db72e
keke <keke@ky-tech.com.cn>
2021-02-27 16:34:23 +0800

    字符串

--- MyKotlin/app/src/main/java/com/kye/android/sdk/kotlin/MainActivity.kt
+++ MyKotlin/app/src/main/java/com/kye/android/sdk/kotlin/MainActivity.kt
@@ -22,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         a = 2
         val s2 = "${s1.replace("is", "was")},but now is $a"
         Log.i(TAG, s2)
+        //类型后面加?表示可为空
+        var age: String? = "23"
+        //抛出空指针异常
+        val ages = age!!.toInt()
+        //不做处理返回 null
+        val ages1 = age?.toInt()
+        //age为空返回-1
+        val ages2 = age?.toInt() ?: -1
     }
 
     fun vars(vararg v: Int) {
