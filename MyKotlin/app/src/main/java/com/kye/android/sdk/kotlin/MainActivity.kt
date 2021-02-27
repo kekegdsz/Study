030a5732ad7a1636a7e21e1089e713cdf0a65608
keke <keke@ky-tech.com.cn>
2021-02-27 16:19:29 +0800

    基础数据

--- MyKotlin/app/src/main/java/com/kye/android/sdk/kotlin/MainActivity.kt
+++ MyKotlin/app/src/main/java/com/kye/android/sdk/kotlin/MainActivity.kt
@@ -2,10 +2,39 @@ package com.kye.android.sdk.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test()
+        var num = sum(5, 7)
+        Log.i(TAG, num?.toString())
+        vars(1, 2, 3, 4, 5)
+        //模版
+        var a = 1
+        val s1 = "a is $a"
+        Log.i(TAG, s1)
+        a = 2
+        val s2 = "${s1.replace("is", "was")},but now is $a"
+        Log.i(TAG, s2)
+    }
+
+    fun vars(vararg v: Int) {
+        for (vt in v) {
+            Log.i(TAG, vt.toString())
+        }
+    }
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun test() {
+        Log.i(TAG, "test")
     }
 }
