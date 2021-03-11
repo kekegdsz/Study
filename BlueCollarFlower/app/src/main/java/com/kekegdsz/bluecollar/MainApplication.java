4fd95f7cad9f87ce1ab8cdb63100f7830f75b199
keke <keke@ky-tech.com.cn>
2021-03-11 17:01:25 +0800

    移除阿里百川api

--- BlueCollarFlower/app/src/main/java/com/kekegdsz/bluecollar/MainApplication.java
+++ BlueCollarFlower/app/src/main/java/com/kekegdsz/bluecollar/MainApplication.java
@@ -1,6 +1,10 @@
 package com.kekegdsz.bluecollar;
 
 import android.app.Application;
+import android.widget.Toast;
+
+import com.alibaba.baichuan.android.trade.AlibcTradeSDK;
+import com.alibaba.baichuan.android.trade.callback.AlibcTradeInitCallback;
 
 public class MainApplication extends Application {
 
@@ -13,6 +17,18 @@ public class MainApplication extends Application {
     }
 
     private void initStoreSdk() {
+        AlibcTradeSDK.asyncInit(this, new AlibcTradeInitCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainApplication.this, "初始化成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Toast.makeText(MainApplication.this, "初始化失败:" + msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
 //        String pid = "mm_25717530_2251950174_111232300350"; // 淘宝联盟pid
 //        String adzoneid = "111232300350"; // pid最后一段
 //        String unionId = ""; // 联盟id（一般传""）
